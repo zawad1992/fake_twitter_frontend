029405d93c1133c37547c1f72f0945a2863604e8
Zawadul Kawum <zawad1992@gmail.com>
2023-11-22 00:00:37 +0600

    Update HomeView.vue with new tweet functionality

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -41,16 +41,15 @@
       <div class="col-md-5">
         <div class="card">
           <div class="card-body">
-            <h5 class="card-title">Latest Tweets</h5>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor
-              mauris
-              vitae
-              nunc
-              aliquam, id lacinia nunc tincidunt. Sed auctor, nunc id aliquet tincidunt, nunc nunc
-              tincidunt
-              mauris,
-              id
-              aliquam nunc nunc id.</p>
+            <h5 class="card-title">Share your thought</h5>
+            <p class="card-text">
+              <form @submit.prevent="addTweet">
+                <div class="mb-3">
+                  <textarea class="form-control" v-model="newTweet.tweet" id="tweet" rows="3"></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary w-100">Tweet</button>
+              </form>
+            </p>
           </div>
         </div>
         <div class="tab-content mt-2" id="myTabContent">
@@ -149,11 +148,72 @@
   </div>
 </template>
 <script>
+import axios from 'axios';
+
 export default {
   data() {
+    let userData = sessionStorage.getItem('user');
+    let user;
+
+    if (userData !== null) {
+        // Parse userData if it's not null
+        try {
+            user = JSON.parse(userData);
+        } catch (error) {
+            console.error("Error parsing user data from sessionStorage:", error);
+            user = {};
+        }
+    } else {
+        // Set user to an empty object if userData is null
+        user = {};
+    }
+
     return {
+      tweets: [],
+      newTweet: {
+        user_id: user._id || '', // Set the user ID here or dynamically
+        tweet: ''
+      },
       currentTab: 'foryou'
+    };
+  },
+  async created() {
+    await this.fetchTweets();
+  },
+  methods: {
+    async fetchTweets() {
+      try {
+        const auth = JSON.parse(sessionStorage.getItem('authorisation'));
+        const token = auth ? auth.token : '';
+
+        const response = await axios.get('http://127.0.0.1:8888/LARAVEL/fake_twitter_backend/api/tweets', {
+          headers: {
+            'Authorization': `Bearer ${token}`
+          }
+        });
+
+        this.tweets = response.data;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async addTweet() {
+      try {
+        const auth = JSON.parse(sessionStorage.getItem('authorisation'));
+        const token = auth ? auth.token : '';
+
+        const response = await axios.post('http://127.0.0.1:8888/LARAVEL/fake_twitter_backend/api/tweets', this.newTweet, {
+          headers: {
+            'Authorization': `Bearer ${token}`
+          }
+        });
+
+        this.tweets.unshift(response.data);
+        this.newTweet = { user_id: this.newTweet.user_id, tweet: '' }; // Reset the tweet text but keep the user_id
+      } catch (error) {
+        console.error(error);
+      }
     }
   }
-}
+};
 </script>
