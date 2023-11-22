893497f38464d7985c5be9a14b6a942424472d40
Zawadul Kawum <zawad1992@gmail.com>
2023-11-23 00:36:48 +0600

    Update API base URL

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://127.0.0.1:8888/LARAVEL/fake_twitter_backend/api/';
+axios.defaults.baseURL = 'http://127.0.0.1:8000/api/';
 
 
 function getAuthToken() {
