24d68fde886c72e8eef8bd112da9a2a11066065c
Zawadul Kawum <zawad1992@gmail.com>
2023-11-23 05:25:22 +0600

    Update API base URL

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://twitter.errorpoint.com/api/';
+axios.defaults.baseURL = 'https://twitter.errorpoint.com/backend/api/';
 
 
 function getAuthToken() {
