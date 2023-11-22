e82a5596e569990a1895e9622bad09bdbdf683f4
Zawadul Kawum <zawad1992@gmail.com>
2023-11-22 22:03:03 +0600

    Refactor profile page layout and add user information

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,8 +1,81 @@
 <template>
-  <div class="col-md-12">
-    <h1>This is an profile page</h1>
+  <div class="container profile-page">
+    <h1 class="mb-4">My Profile</h1>
+    <div v-if="userInfo" class="user-info mb-4">
+      <h2>{{ userInfo.name }}</h2>
+      <p>Email: {{ userInfo.email }}</p>
+      <p>Joined: {{ formatDate(userInfo.created_at) }}</p>
+    </div>
+
+    <div class="row">
+      <div v-if="userInfo.followers && userInfo.followers.length" class="col-md-12 followers mb-4">
+        <h3>Followers</h3>
+        <ul class="list-group">
+          <li v-for="(follower, index) in userInfo.followers" :key="follower._id" class="list-group-item">
+            {{ index + 1 }}. {{ follower.follower.name }} ({{ formatDate(follower.created_at) }})
+          </li>
+        </ul>
+      </div>
+
+      <div v-if="userInfo.following && userInfo.following.length" class="col-md-12 following">
+        <h3>Following</h3>
+        <ul class="list-group">
+          <li v-for="(following, index) in userInfo.following" :key="following._id" class="list-group-item">
+            {{ index + 1 }}. {{ following.followed.name }} ({{ formatDate(following.created_at) }})
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 <script>
-console.log("Hello from ProfileView.vue");
+// Import the API functions
+import { apiGet, apiPost, apiLogout } from '../api';
+
+export default {
+  
+  data() {
+    let userData = sessionStorage.getItem('user');
+    let user;
+    if (userData !== null) {
+        // Parse userData if it's not null
+        try {
+            user = JSON.parse(userData);
+        } catch (error) {
+            console.error("Error parsing user data from sessionStorage:", error);
+            user = {};
+        }
+    } else {
+        // Set user to an empty object if userData is null
+        user = {};
+    }
+
+    return {
+      userInfo: [],
+      user_id: user._id || '',
+    };
+  },
+  async created() {
+    await this.fetchUserInfo();
+  },
+  methods: {
+    async fetchUserInfo() {
+      try {
+        // Fetching data for 'For You' tab
+        const userResponse = await apiGet(`/users/${this.user_id}`);
+        if (userResponse && userResponse.data) {
+          this.userInfo = userResponse.data || []; // Accessing nested 'data'
+          console.log(this.userInfo);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    formatDate(dateString) {
+      const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' };
+      return new Date(dateString).toLocaleString('en-US', options);
+    }
+  }
+};
 </script>
+
