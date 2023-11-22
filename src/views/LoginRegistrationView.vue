edb6bde52e20165ae93a6270346c62055540a545
Zawadul Kawum <zawad1992@gmail.com>
2023-11-22 13:09:30 +0600

    Refactor form inputs in LoginRegistrationView.vue

--- src/views/LoginRegistrationView.vue
+++ src/views/LoginRegistrationView.vue
@@ -16,11 +16,11 @@
                       <form @submit.prevent="submitForm">
                         <div class="mb-3">
                           <label for="email" class="form-label">Email</label>
-                          <input type="text" class="form-control" v-model="email" id="email">
+                          <input type="text" class="form-control" v-model="login.email" id="login.email">
                         </div>
                         <div class="mb-3">
                           <label for="password" class="form-label">Password</label>
-                          <input type="password" class="form-control" v-model="password" id="password">
+                          <input type="password" class="form-control" v-model="login.password" id="login.password">
                         </div>
                         <button type="submit" class="btn btn-primary w-100">Login</button>
                       </form>
@@ -36,22 +36,18 @@
                   <h2 class="text-center mb-4">Create your account</h2>
                   <div class="card">
                     <div class="card-body">
-                      <form>
+                      <form @submit.prevent="submitRegistrationForm">
                         <div class="mb-3">
                           <label for="name" class="form-label">Name</label>
-                          <input type="text" class="form-control" id="name">
-                        </div>
-                        <div class="mb-3">
-                          <label for="phone" class="form-label">Phone</label>
-                          <input type="text" class="form-control" id="phone">
+                          <input type="text" class="form-control" v-model="registration.name" id="name">
                         </div>
                         <div class="mb-3">
                           <label for="email" class="form-label">Email</label>
-                          <input type="email" class="form-control" id="email">
+                          <input type="email" class="form-control" v-model="registration.email" id="email">
                         </div>
                         <div class="mb-3">
                           <label for="password" class="form-label">Password</label>
-                          <input type="password" class="form-control" id="password">
+                          <input type="password" class="form-control" v-model="registration.password" id="password">
                         </div>
                         <button type="submit" class="btn btn-primary w-100">Sign up</button>
                       </form>
@@ -71,26 +67,38 @@ export default {
     data() {
         return {
           currentTab: 'login',
-          email: 'zawad1992@gmail.com ',
-          password: '123456'
+          login: {
+            email: 'zawad1992@gmail.com',
+            password: '123456'
+          },
+          registration: {
+            name: '',
+            email: '',
+            password: '123456'
+          }
+         
         }
     },
     methods: {
         async submitForm() {
             try {
-               
-                const response = await axios.post('http://127.0.0.1:8888/LARAVEL/fake_twitter_backend/api/login', {
-                    email: this.email,
-                    password: this.password
-                });
+                const response = await axios.post('http://127.0.0.1:8888/LARAVEL/fake_twitter_backend/api/login', this.login);
                 this.saveToSessionStorage(response.data);
                 this.$router.push('/');
             } catch (error) {
                 console.error(error);
             }
         },
+        async submitRegistrationForm() {
+          try {
+              const response = await axios.post('http://127.0.0.1:8888/LARAVEL/fake_twitter_backend/api/register', this.registration);
+              this.saveToSessionStorage(response.data);
+              this.$router.push('/');
+          } catch (error) {
+              console.error(error);
+          }
+        },
         saveToSessionStorage(data) {
-          console.log(data);
             sessionStorage.setItem('user', JSON.stringify(data.user));
             sessionStorage.setItem('authorisation', JSON.stringify(data.authorisation));
         }
